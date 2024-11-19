80f87bc41f97e73b47dd619028ef1632b3976f2b
Chris O'Brien <obrchr96@gmail.com>
2024-11-18 23:41:08 -0800

    fmt

--- flake.nix
+++ flake.nix
@@ -31,14 +31,15 @@
             inherit system pkgs lib;
             rev = self.rev or self.dirtyRev or null;
           };
-          todo = 
+          todo =
             trace ''
               eureka's todo list:
                 - Add and configure home-manager
                 - Include flake in main dotfiles repo
                 - Add x86_64 linux builder and rosetta for aarch64-darwin system
                 - Enable fingerprint auth
-            '' true;
+            ''
+              true;
         in
         {
           # Rebuild darwin flake using:
